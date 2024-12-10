7f871840e1a65478b345877acec84b4c334acf00
Prosono <norman576@hotmail.com>
2024-12-10 15:29:49 +0100

    chaned some files
    filename change

--- custom_components/smarti/__init__.py
+++ custom_components/smarti/__init__.py
@@ -16,9 +16,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = {}
 
-    # Forward the setup to the update platform
+    # Forward the setup to the updater platform
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "update")
+        hass.config_entries.async_forward_entry_setup(entry, "updater")
     )
 
     return True
@@ -27,24 +27,23 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload a config entry."""
     _LOGGER.info("Unloading SMARTi config entry.")
 
-    await hass.config_entries.async_forward_entry_unload(entry, "update")
+    await hass.config_entries.async_forward_entry_unload(entry, "updater")
 
     hass.data[DOMAIN].pop(entry.entry_id)
     return True
 
 async def async_migrate_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Handle migration of config entry if needed."""
+    """Handle migration of the config entry if needed."""
     _LOGGER.info(f"Migrating SMARTi entry from version {entry.version}")
 
-    # Define the current version of your integration
     current_version = 1
 
     if entry.version == current_version:
         _LOGGER.info("No migration necessary")
         return True
 
-    # Implement migration logic if needed
-    # For now, we'll assume no changes are needed and simply update the version
+    # Implement migration logic if needed.
+    # For now, we'll just ensure the entry version is up to date.
     hass.config_entries.async_update_entry(entry, version=current_version)
     _LOGGER.info(f"Migration to version {current_version} successful")
     return True
